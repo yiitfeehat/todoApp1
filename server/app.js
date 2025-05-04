003672bd41f0acad2c90acd359719f19c0175a3b
Ferhat Yigit <yiitferhat@gmail.com>
2025-05-04 16:41:17 +0300

    up

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ app.use(express.json())
 //   }
 const cors = require('cors');
 app.use(cors({
-    origin: ['htttps://example.com', 'http://localhost:3000', "https://todoapp1-wvz1.onrender.com/"], // Allow these origins
+    origin: ['htttps://example.com', 'http://localhost:3000', "https://todoapp1-wvz1.onrender.com"], // Allow these origins
 }));
 /* ------------------------------------------------------- */
 // ROUTERS:
