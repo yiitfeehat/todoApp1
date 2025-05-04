f695aef33651514c9504482b6f87c2d3550e2778
Ferhat Yigit <yiitferhat@gmail.com>
2025-05-04 17:51:52 +0300

    up

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
 import axios from "axios";
-
+ 
 //! component LifeCycle
 //* 1-render
 //* 2-componentDidMount => useEffect(()=>{},[])
